dc93c80dd751130ab97c770fb5dbde1476fc9730
Javian Anderson <javian99anderson@gmail.com>
2020-11-18 03:07:06 -0500

    Exercise 3 Finished

--- world.php
+++ world.php
@@ -9,23 +9,20 @@ $stmt = $conn->query("SELECT * FROM countries");
 
 $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-// filter_var($_GET['country'], FILTER_SANITIZE_STRING);
+filter_var($_GET['country'], FILTER_SANITIZE_STRING);
 $country= $_GET['country'];
 //Get country to search for
 
-if(isset($country) == true)
-{
-    $stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%'");
-  
-    
-    echo '<ul>';
-    foreach ($results as $row) {
-      echo '<li>' . $row['name'] . ' is ruled by ' . $row['head_of_state'] . '</li>';
-    }
-    echo '</ul>';
+if(isset($country)){
+  $stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%'");
+  $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+  echo '<ul>';
+  foreach ($results as $row){
+    echo '<li>' . $row['name'] . ' is ruled by ' . $row['head_of_state'] . '</li>';
+  }
+  echo '</ul>';
 }
-else
-{
-    echo 'batty ';
+else{
+  echo 'batty ';
 }
 ?>
