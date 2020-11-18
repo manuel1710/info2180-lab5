b924d4508274312961b38b070559e50eee0e55be
Javian Anderson <javian99anderson@gmail.com>
2020-11-17 21:34:52 -0500

    Try a thing

--- world.php
+++ world.php
@@ -1,7 +1,7 @@
 <?php
 $host = 'localhost';
 $username = 'lab5_user';
-$password = '';
+$password = 'password123';
 $dbname = 'world';
 
 $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
@@ -10,8 +10,21 @@ $stmt = $conn->query("SELECT * FROM countries");
 $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
 ?>
-<ul>
-<?php foreach ($results as $row): ?>
-  <li><?= $row['name'] . ' is ruled by ' . $row['head_of_state']; ?></li>
+<?php
+$sql = "SELECT * FROM countries WHERE name LIKE '%$country%'";
+?>
+<?php filter_var($_GET['country'], FILTER_SANITIZE_STRING);?>
+
+
+<!-- <ul> -->
+<!-- <?php foreach ($results as $row): ?> -->
+  <!-- <li><?= $row['name'] . ' is ruled by ' . $row['head_of_state']; ?></li> -->
+<!-- <?php endforeach; ?> -->
+<!-- </ul> -->
+
+<?php foreach ($results as $row): ?> 
+  <?php if($_GET['country'] == $row['name']):?>
+    <?= $row['name'] . ' is ruled by ' . $row['head_of_state']; ?>
+  <?php endif;?>
 <?php endforeach; ?>
-</ul>
+
