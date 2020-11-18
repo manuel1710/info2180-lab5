a88b34a1d480d41ee539be5fbf91319ac63f4534
Javian Anderson <javian99anderson@gmail.com>
2020-11-18 14:22:43 -0500

    Completed Exercise 5

--- world.php
+++ world.php
@@ -1,5 +1,5 @@
 <?php
-$host = getenv('IP');
+$host = 'localhost';
 $username = 'lab5_user';
 $password = 'password123';
 $dbname = 'world';
@@ -11,18 +11,52 @@ $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
 filter_var($_GET['country'], FILTER_SANITIZE_STRING);
 $country= $_GET['country'];
+// filter_var($_GET['context'], FILTER_SANITIZE_STRING);
+// $cities = $_GET['context'];
 //Get country to search for
 
-if(isset($country)){
+if(isset($country)==true && isset($_GET['context'])==false){
   $stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%'");
   $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
-  echo '<ul>';
+  echo "<table>
+  <tr>
+    <th>Name</th>
+    <th>Continent</th>
+    <th>Independence</th>
+    <th>Head of State</th>
+  </tr>";
   foreach ($results as $row){
-    echo '<li>' . $row['name'] . ' is ruled by ' . $row['head_of_state'] . '</li>';
+  echo "<tr>";
+  echo "<td>".$row['name']."</td>";
+  echo "<td>".$row['continent']."</td>";
+  echo "<td>".$row['independence_year']."</td>";
+  echo "<td>".$row['head_of_state']."</td>";
+  echo "</tr>";
   }
-  echo '</ul>';
+  // endforeach
+  echo "</table>";
+}
+elseif(isset($country)==true && isset($_GET['context'])==true){
+  $stmt = $conn->query("SELECT cities.name, cities.district,cities.population FROM countries INNER JOIN cities ON countries.code = cities.country_code WHERE countries.name LIKE '%$country%'");
+  $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+  echo "<table>
+  <tr>
+    <th>Name</th>
+    <th>District</th>
+    <th>Population</th>
+  </tr>";
+  foreach ($results as $row){
+    echo "<tr>";
+    echo "<td>".$row['name']."</td>";
+    echo "<td>".$row['district']."</td>";
+    echo "<td>".$row['population']."</td>";
+    echo "</tr>";
+    }
+  // endforeach;
+  echo "</table>";
+
 }
 else{
-  echo 'batty ';
+  echo 'ERROR!!!';
 }
 ?>
